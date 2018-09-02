55f6c354b4050695a991ba5b46f4325e44c6140c
Brandon James Talbot <bjtal91@gmail.com>
2018-09-01 23:01:27 -0700

    log4j2 it up.

--- DJBotDiscord/src/main/kotlin/org/darkjaguar/djbotdiscord/logging/ansi/AnsiElement.kt
+++
@@ -1,5 +0,0 @@
-package org.darkjaguar.djbotdiscord.logging.ansi
-
-interface AnsiElement {
-    override fun toString(): String
-}
--- DJBotDiscord/src/main/kotlin/org/darkjaguar/djbotdiscord/logging/ansi/AnsiStyle.kt
+++
@@ -1,9 +0,0 @@
-package org.darkjaguar.djbotdiscord.logging.ansi
-
-enum class AnsiStyle(val code: String) : AnsiElement {
-    NORMAL("0"),
-    BOLD("1"),
-    FAINT("2"),
-    ITALIC("3"),
-    UNDERLINE("4")
-}
